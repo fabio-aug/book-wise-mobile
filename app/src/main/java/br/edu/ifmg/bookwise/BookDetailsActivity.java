0cd3f4fe344a3004c1140fa3b23087162fe786a4
luanaassis <luanaassis0908@gmail.com>
2023-08-20 12:53:12 -0300

    hf

--- app/src/main/java/br/edu/ifmg/bookwise/BookDetailsActivity.java
+++ app/src/main/java/br/edu/ifmg/bookwise/BookDetailsActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ifmg.bookwise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,15 @@ import br.edu.ifmg.bookwise.databinding.BookDetailsBinding;
 
 public class BookDetailsActivity extends AppCompatActivity {
     public static final String ID = "BookDetailsActivity_Book_Id";
+    public static final String AUTHOR = "BookDetailsActivity_Book_Author";
+    public static final String AVERAGEREVIEW = "BookDetailsActivity_Book_AverageReview";
+    public static final String TITLE = "BookDetailsActivity_Book_Title";
+    public static final String SYNOPSIS = "BookDetailsActivity_Book_Synopsis";
+    public static final String IMAGE = "BookDetailsActivity_Book_Image";
+    public static final String SHARING = "BookDetailsActivity_Book_Sharing";
+    public static final String GENDERS = "BookDetailsActivity_Book_Genders";
+    public static final String REVIEWS = "BookDetailsActivity_Book_Reviews";
+
     TextView bookTitle, author, synopsis, genders, averageReview;
     Button share;
     ImageView image;
@@ -22,6 +32,7 @@ public class BookDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         BookDetailsBinding binding = BookDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Intent intent = getIntent();
         bookTitle = findViewById(R.id.booktitle);
         author = findViewById(R.id.author);
         synopsis = findViewById(R.id.synopsis);
--- app/src/main/java/br/edu/ifmg/bookwise/HomeActivity.java
+++ app/src/main/java/br/edu/ifmg/bookwise/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import br.edu.ifmg.bookwise.classes.Book;
+import br.edu.ifmg.bookwise.classes.Gender;
 import br.edu.ifmg.bookwise.databinding.HomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
@@ -13,6 +14,14 @@ public class HomeActivity extends AppCompatActivity {
     private void openBookDetails(Book book) {
         Intent intent = new Intent(this, BookDetailsActivity.class);
         intent.putExtra(BookDetailsActivity.ID, book.id);
+        intent.putExtra(BookDetailsActivity.AUTHOR, book.author);
+        intent.putExtra(BookDetailsActivity.AVERAGEREVIEW, book.averageReview);
+        intent.putExtra(BookDetailsActivity.TITLE, book.title);
+        intent.putExtra(BookDetailsActivity.SYNOPSIS, book.synopsis);
+        intent.putExtra(BookDetailsActivity.IMAGE, book.image);
+        intent.putExtra(BookDetailsActivity.SHARING, book.sharing);
+        intent.putExtra(BookDetailsActivity.GENDERS, book.genders);
+        intent.putExtra(BookDetailsActivity.REVIEWS, book.reviews);
         startActivity(intent);
     }
 
@@ -24,6 +33,14 @@ public class HomeActivity extends AppCompatActivity {
 
         Book book = new Book();
         book.id = 1;
+        book.author = "Luana Assis";
+        book.averageReview = 2.5f;
+        book.title = "O Segredo de Feiurinha";
+        book.synopsis = "A princesa Feiurinha sumiu misteriosamente! Como sua história não é muito conhecida pelas crianças, ela corre o risco de ser esquecida para sempre. Mas, para que isso não aconteça, todas as princesas encantadas se juntam para salvar a princesinha.";
+        book.image = "image";
+        book.genders = null;
+        book.sharing = 1;
+        book.reviews = null;
 
         openBookDetails(book);
     }
